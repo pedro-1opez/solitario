2304c6390d222e123040b99648f471588c54f389
Pedro Lopez <peanlolo@hotmail.com>
2023-02-15 09:23:58 -0700

    Creacion del metodo obten_carta() y obten_mano()

--- funciones.py
+++ funciones.py
@@ -1,3 +1,5 @@
+from random import choice
+
 def generar_mazo():
     numeros = [f"{x}" for x in range(2,11)] 
     letras = ["J","Q","K","A"] 
@@ -14,3 +16,19 @@ def generar_mazo():
     mazo.extend(jokers)
 
     return mazo
+
+def obten_carta(mazo):
+    carta = choice(mazo)
+    mazo.remove(carta)
+
+    return carta
+
+def obten_mano(baraja:list) -> list:    
+    
+    mano = []
+
+    for i in range(0,5):
+        carta = obten_carta(baraja)
+        mano.append(carta)
+        
+    return mano
--- main.py
+++ main.py
@@ -4,7 +4,14 @@ def main():
 
     #Genera el mazo
     mazo = funciones.generar_mazo()
-        
+
+    #Genera la mano del jugador
+    jugador = funciones.obten_mano(mazo)
+    jugador = sorted(jugador)
+    
+    #Genera la mano del CPU
+    cpu = funciones.obten_mano(mazo)
+    cpu = sorted(cpu)        
 
 if __name__ == "__main__":
     main()    
